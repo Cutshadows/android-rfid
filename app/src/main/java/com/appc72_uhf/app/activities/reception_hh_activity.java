0031ef11699d451fd56abee5730b6f27529ec271
Cutshadows <ramnstein_1749_@hotmail.com>
2020-12-07 17:54:29 -0300

    habilitando pantallas de recepcion en hh

--- app/src/main/java/com/appc72_uhf/app/activities/reception_hh_activity.java
+++ app/src/main/java/com/appc72_uhf/app/activities/reception_hh_activity.java
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.appc72_uhf.app.R;
 
-public class DetailMakeLabelsActivity extends AppCompatActivity {
+public class reception_hh_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail_make_labels);
+        setContentView(R.layout.activity_reception_hh_activity);
     }
 }
